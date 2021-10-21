e8334b679b61cb262d9c71f6d45f579a1faf1fb4
mirrorz692 <92586690+mirrorz692@users.noreply.github.com>
2021-10-21 18:29:05 +0600

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
+import os
+
 print("Starting")
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-  unzip ngrok-stable-linux-386.zip
-./ngrok authtoken 1zjUyneuN2WVwmRvii8G83KK5cH_5LxMMFUzWrkysJzGvmhBU
-./ngrok http 80
+os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip")
+os.system("unzip ngrok-stable-linux-386.zip")
+os.system("./ngrok authtoken 1zjUyneuN2WVwmRvii8G83KK5cH_5LxMMFUzWrkysJzGvmhBU")
+os.system("./ngrok http 80")
 print("Successful")
